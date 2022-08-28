9bcfc1228e21b436d016fb801d4ac4a02044f85a
vops <gamestateyt@gmail.com>
2022-08-28 18:04:34 -0400

    Update checkforupdate.js

--- utils/checkforupdate.js
+++ utils/checkforupdate.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 let installedVer = package.version;
 
 // check for version update
-let latestData = fetch('https://raw.githubusercontent.com/ItsVops/Vops-Capes/main/package.json').json();
+let latestData = fetch('https://raw.githubusercontent.com/vopswtf/Vops-Capes/main/package.json').json();
 if (installedVer !== latestData.version) {
     console.log("WARNING!\nWARNING! --- THIS IS AN OLD VERSION OF VOPS CAPES AND MAY BE MISSING CORE FEATURES\nWARNING! --- UPDATE HERE: https://github.com/ItsVops/Vops-Capes\nWARNING!");
 }
@@ -24,8 +24,8 @@ itemList.forEach(item => {
 if (missingItems.length > 0) {
     console.log("[UPDATER] You seem to be missing some items. Downloading...")
     missingItems.forEach(item => {
-        let texture = fetch(`https://raw.githubusercontent.com/ItsVops/Vops-Capes/main/api/assets/items/${item}/texture.png`).buffer();
-        let model = fetch(`https://raw.githubusercontent.com/ItsVops/Vops-Capes/main/api/assets/items/${item}/model.cfg`).buffer();
+        let texture = fetch(`https://raw.githubusercontent.com/vopswtf/Vops-Capes/main/api/assets/items/${item}/texture.png`).buffer();
+        let model = fetch(`https://raw.githubusercontent.com/vopswtf/Vops-Capes/main/api/assets/items/${item}/model.cfg`).buffer();
         fs.mkdirSync("./api/assets/items/" + item)
         fs.writeFileSync("./api/assets/items/" + item + "/texture.png", texture)
         fs.writeFileSync("./api/assets/items/" + item + "/model.cfg", model)
